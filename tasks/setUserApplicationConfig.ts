856e120800e8c2dc990502a85f00117f2241ca80
0xIryna <ihubernska@gmail.com>
2023-02-05 23:01:44 -0800

    config task update

--- tasks/setUserApplicationConfig.ts
+++ tasks/setUserApplicationConfig.ts
@@ -10,7 +10,6 @@ const UA_ADDRESSES = require("../constants/uaAddresses.json")
 const LZ_ENDPOINTS = require("../constants/layerzeroEndpoints.json")
 
 const contractName = "ILayerZeroUserApplicationConfig"
-const endpointContractName = "ILayerZeroEndpoint"
 const VERSION = 2
 
 // Application config types from UltraLightNodeV2 contract
@@ -53,45 +52,68 @@ module.exports = async (taskArgs, hre) => {
     await Promise.all(localNetworks.map(async (localNetwork) => {
         const transactions: Transaction[] = []
         const uaAddress = UA_ADDRESSES[localNetwork]
+        
         if (uaAddress === undefined) return
+        
         const endpoint = await getContractAt(hre, localNetwork, "Endpoint", endpointAbi, LZ_ENDPOINTS[localNetwork])
         const ua = await getContractAt(hre, localNetwork, "UserApplication", endpointAbi, uaAddress)
         const chainId = CHAIN_ID[localNetwork]
         const localNetworkConfig = config[localNetwork]
+        
         if (localNetworkConfig === undefined) return
+        const uaConfig = await endpoint.uaConfigLookup(ua.address)
 
-        transactions.push(...(await setSendVersion(chainId, endpoint, ua, localNetworkConfig.sendVersion)))
+        if (localNetworkConfig.sendVersion !== undefined) {
+            transactions.push(...(await setSendVersion(chainId, ua, uaConfig[0].toNumber(), localNetworkConfig.sendVersion)))
+        }
+
+        if (localNetworkConfig.receiveVersion !== undefined) {
+            transactions.push(...(await setReceiveVersion(chainId, ua, uaConfig[1].toNumber(), localNetworkConfig.receiveVersion)))
+        }
 
         await Promise.all(remoteNetworks.map(async remoteNetwork => {
             if (localNetwork === remoteNetwork || UA_ADDRESSES[remoteNetwork] === undefined) return
             const remoteChainId = CHAIN_ID[remoteNetwork]
 
-            
-            // transactions.push(...(await setTrustedRemote(hre, localNetwork, localContractName, remoteNetwork, remoteContractName, taskArgs.e)))
-            // transactions.push(...(await setTrustedRemote(hre, localNetwork, localContractName, remoteNetwork, remoteContractName, taskArgs.e)))
-            // transactions.push(...(await setTrustedRemote(hre, localNetwork, localContractName, remoteNetwork, remoteContractName, taskArgs.e)))
-            // transactions.push(...(await setTrustedRemote(hre, localNetwork, localContractName, remoteNetwork, remoteContractName, taskArgs.e)))
-          
+            if (localNetworkConfig.inboundProofLibraryVersion !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_INBOUND_PROOF_LIBRARY_VERSION, "uint16", localNetworkConfig.inboundProofLibraryVersion)))
+            }
+
+            if (localNetworkConfig.inboundBlockConfirmations !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_INBOUND_BLOCK_CONFIRMATIONS, "uint16", localNetworkConfig.inboundBlockConfirmations)))
+            }
+
+            if (localNetworkConfig.relayer !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_RELAYER, "address", localNetworkConfig.relayer)))
+            }
+
+            if (localNetworkConfig.outboundProofType !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_OUTBOUND_PROOF_TYPE, "uint16", localNetworkConfig.outboundProofType)))
+            }
+
+            if (localNetworkConfig.outboundBlockConfirmations !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_OUTBOUND_BLOCK_CONFIRMATIONS, "uint16", localNetworkConfig.outboundBlockConfirmations)))
+            }
+
+            if (localNetworkConfig.oracle !== undefined) {
+                transactions.push(...(await setConfig(chainId, remoteChainId, ua, CONFIG_TYPE_ORACLE, "address", localNetworkConfig.oracle)))
+            }          
         }))
     }))
 }
 
-const setSendVersion = async (chainId: string, endpoint: any, ua: any, newSendVersion: any): Promise<Transaction[]> => {
-    const uaConfig = await endpoint.uaConfigLookup(ua.address)
-    const currentSendVersion = uaConfig[0].toNumber()
+const setSendVersion = async (chainId: string, ua: any, currentSendVersion: any, newSendVersion: any): Promise<Transaction[]> => {
     const needChange = currentSendVersion !== newSendVersion
     const contractAddress = ua.address
     const methodName = "setSendVersion"
     const args = [newSendVersion]
     const calldata = ua.interface.encodeFunctionData(methodName, args)
     const diff = needChange ? { oldValue: currentSendVersion, newValue: newSendVersion } : undefined
-    
+
     return [{ needChange, chainId, contractName, contractAddress, methodName, args, calldata, diff }]
 }
 
-const setReceiveVersion = async (chainId: string, endpoint: any, ua: any, newReceiveVersion: any): Promise<Transaction[]> => {
-    const uaConfig = await endpoint.uaConfigLookup(ua.address)
-    const currentReceiveVersion = uaConfig[1].toNumber()
+const setReceiveVersion = async (chainId: string, ua: any, currentReceiveVersion: any, newReceiveVersion: any): Promise<Transaction[]> => {
     const needChange = currentReceiveVersion !== newReceiveVersion
     const contractAddress = ua.address
     const methodName = "setReceiveVersion"
@@ -101,7 +123,6 @@ const setReceiveVersion = async (chainId: string, endpoint: any, ua: any, newRec
 
     return [{ needChange, chainId, contractName, contractAddress, methodName, args, calldata, diff }]
 }
-
 
 const setConfig = async (chainId: string, remoteChainId: number, ua: any, configType: number, configValueType: string, newValue: any): Promise<Transaction[]> => {
     const currentConfig = await ua.getConfig(VERSION, remoteChainId, constants.AddressZero, configType)
